56041670bdc297608d90c83da9d46fef08eb98a5
Navin Ojha <navinojha1996@gmail.com>
2021-02-03 17:49:09 +0530

    Added Logic for Highscore and Reset game

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number!';
-
-const randNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = randNumber;
+let randNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
-  } else if (randNumber > guess) {
-    document.querySelector('.message').textContent = 'Too low!!';
-  } else if (randNumber < guess) {
-    document.querySelector('.message').textContent = 'Too High!!';
-  } else {
-    document.querySelector('.message').textContent = 'Hurray!! Correct Number!';
   }
+  // When the guess is smaller than random number
+  else if (randNumber > guess) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // When the guess is greater than random number
+  else if (randNumber < guess) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
+  }
+  // When the player actually wins the game
+  else {
+    document.querySelector('.message').textContent = 'Hurray!! Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = randNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  randNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = randNumber;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
 });
